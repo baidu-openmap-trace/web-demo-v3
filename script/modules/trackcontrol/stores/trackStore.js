6656c0fe25bebae1eed23ce08fbed90191bd70a5
cuijian03 <cuijian03@baidu.com>
2016-12-08 12:00:39 +0800

    fix bug

--- script/modules/trackcontrol/stores/trackStore.js
+++ script/modules/trackcontrol/stores/trackStore.js
@@ -278,42 +278,43 @@ var TrackStore = Reflux.createStore({
 
         Urls.jsonp(Urls.searchEntity, params, function(data) {
             if (data.status === 0) {
-                data.entities.length = 1;
                 data.entities.map(function(item) {
-                    var point = data.entities[0].latest_location;
-                    var paramsGeo = {
-                        location: point.latitude + ',' + point.longitude,
-                        output: 'json'
-                    };
-                    Urls.jsonp(Urls.getAddress, paramsGeo, function(dataGeo) {
-                        var temp = [];
-                        that.data.column_key.map(function(keyitem, index) {
-                            if (keyitem === '_provider') {
-                                
-                            } else {
-                                temp[index] = [that.data.column[index] + ':', item[keyitem] !== undefined ? item[keyitem] : '无'];
-                            }
+                    if (item.entity_name === entity_name) {
+                        var point = data.entities[0].latest_location;
+                        var paramsGeo = {
+                            location: point.latitude + ',' + point.longitude,
+                            output: 'json'
+                        };
+                        Urls.jsonp(Urls.getAddress, paramsGeo, function(dataGeo) {
+                            var temp = [];
+                            that.data.column_key.map(function(keyitem, index) {
+                                if (keyitem === '_provider') {
+                                    
+                                } else {
+                                    temp[index] = [that.data.column[index] + ':', item[keyitem] !== undefined ? item[keyitem] : '无'];
+                                }
+                            });
+                            temp = temp.filter(function(item) {
+                                return item;
+                            });
+                            var lnglat = item.latest_location.longitude.toFixed(2) + ',' + item.latest_location.latitude.toFixed(2);
+                            that.data.selectCompleteEntities.push({
+                                point: [item.latest_location.longitude, item.latest_location.latitude],
+                                entity_name: item.entity_name,
+                                direction:item.latest_location.direction,
+                                infor: [
+                                    ['状态:', Commonfun.getInfoWindowStatus(item.latest_location.speed, item.latest_location.loc_time, item.latest_location.direction)],
+                                    ['地址:', dataGeo.result.formatted_address === '' ? '无' : dataGeo.result.formatted_address],
+                                    ['定位:', lnglat],
+                                    ['时间:', Commonfun.getLocalTime(item.latest_location.loc_time)]
+                                ].concat(temp)
+                            });
+                            that.data.selectCompleteEntities[0].entity_status = entity_status;
+                            that.data.selectCompleteEntities[0].entity_type = entity_type;
+                            that.data.selectCar = that.data.selectCompleteEntities[0];
+                            that.trigger('selectcardata',that.data.selectCompleteEntities[0]);
                         });
-                        temp = temp.filter(function(item) {
-                            return item;
-                        });
-                        var lnglat = item.latest_location.longitude.toFixed(2) + ',' + item.latest_location.latitude.toFixed(2);
-                        that.data.selectCompleteEntities.push({
-                            point: [item.latest_location.longitude, item.latest_location.latitude],
-                            entity_name: item.entity_name,
-                            direction:item.latest_location.direction,
-                            infor: [
-                                ['状态:', Commonfun.getInfoWindowStatus(item.latest_location.speed, item.latest_location.loc_time, item.latest_location.direction)],
-                                ['地址:', dataGeo.result.formatted_address === '' ? '无' : dataGeo.result.formatted_address],
-                                ['定位:', lnglat],
-                                ['时间:', Commonfun.getLocalTime(item.latest_location.loc_time)]
-                            ].concat(temp)
-                        });
-                        that.data.selectCompleteEntities[0].entity_status = entity_status;
-                        that.data.selectCompleteEntities[0].entity_type = entity_type;
-                        that.data.selectCar = that.data.selectCompleteEntities[0];
-                        that.trigger('selectcardata',that.data.selectCompleteEntities[0]);
-                    });   
+                    }  
                 });
                
             } else {
